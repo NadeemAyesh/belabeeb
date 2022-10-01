ee776ecd00269c18ad227015c57e49f144b3f5ef
Nadeem Ayesh <35177414+NadeemAyesh@users.noreply.github.com>
2022-10-01 17:03:30 +0300

    update vertical slider and hero

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,38 @@
 /*global $, jQuery, console, alert, prompt */
 $(document).ready(function () {
     "use strict";
+
+    if( $('.vertical-slider').length != 0 ) {
+        const swiper = new Swiper('.vertical-slider', {
+            // Optional parameters
+            direction: 'horizontal',
+            // loop: true,
+
+            breakpoints: {
+                0: {
+
+                },
+                576: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+                },
+                768: {
+                  slidesPerView: 4,
+                  spaceBetween: 40,
+                },
+                1024: {
+                  slidesPerView: 3,
+                  spaceBetween: 30,
+                },
+            },
     
+            // Navigation arrows
+            navigation: {
+                nextEl: '.vertical-arrows .swiper-button-next',
+                prevEl: '.vertical-arrows .swiper-button-prev',
+            },
+        });
+    }
+
 });
+
